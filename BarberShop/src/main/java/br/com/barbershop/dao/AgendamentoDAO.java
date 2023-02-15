32de060cb0729ec0728373755b44fca8a8a835c2
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-02-14 22:30:02 -0300

    update

--- BarberShop/src/main/java/br/com/barbershop/dao/AgendamentoDAO.java
+++ BarberShop/src/main/java/br/com/barbershop/dao/AgendamentoDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.barbershop.dao;
 
 import java.sql.Connection;
@@ -24,10 +19,6 @@ import br.com.barbershop.model.Usuario;
 import br.com.barbershop.util.ConexaoBD;
 import br.com.barbershop.view.Agenda;
 
-/**
- *
- * @author jefferson.silva
- */
 public class AgendamentoDAO {
 	
 	 private Connection conexao = null;
--- BarberShop/src/main/java/br/com/barbershop/dao/ServiceDAO.java
+++ BarberShop/src/main/java/br/com/barbershop/dao/ServiceDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.barbershop.dao;
 
 import java.sql.Connection;
@@ -16,10 +11,6 @@ import javax.swing.JOptionPane;
 import br.com.barbershop.model.Service;
 import br.com.barbershop.util.ConexaoBD;
 
-/**
- *
- * @author jefferson.silva
- */
 public class ServiceDAO {
 	
 	private Connection conexao = null;
